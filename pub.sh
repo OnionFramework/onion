45bb8059d3e886825d16350530c86034ca4017fe
OnionFramework at Imac <onion.mhframework@gmail.com>
2023-02-23 13:52:26 -0300

    removed

--- pub.sh
+++
@@ -1,9 +0,0 @@
-if [ "$1" == "" ] || [ "$2" == "" ]; then 
-    echo "USAGE: ./pub.sh <file> <commit message>"
-    exit 0
-fi
-
-git add $1
-echo "git commit -m \"$2\""
-#git commit -m \"$2\"
-#git push
