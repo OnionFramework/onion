34b1522f8ba0c78a3bec49368aa37940ce38205e
OnionFramework <onion.mhframework@gmail.com>
2023-02-23 13:38:57 -0300

    Shell script to commit files individually

---
+++ pub.sh
@@ -0,0 +1,9 @@
+if [ "$1" == "" ] || [ "$2" == "" ]; then 
+    echo "USAGE: ./pub.sh <file> <commit message>"
+    exit 0
+fi
+
+git add $1
+echo "git commit -m \"$2\""
+#git commit -m \"$2\"
+#git push
