fa5e1e03ba91bd97d8da4e03898b73b9e6dde26f
OnionFramework at Imac <onion.mhframework@gmail.com>
2023-02-23 15:46:11 -0300

    Improved documentation

--- doc/html/namespaceonion.js
+++ doc/html/namespaceonion.js
@@ -2,17 +2,13 @@ var namespaceonion =
 [
     [ "algorithms", "namespaceonion_1_1algorithms.html", "namespaceonion_1_1algorithms" ],
     [ "cops", "namespaceonion_1_1cops.html", "namespaceonion_1_1cops" ],
-    [ "ComponentData", "classonion_1_1_component_data.html", "classonion_1_1_component_data" ],
-    [ "SetComponent", "classonion_1_1_set_component.html", "classonion_1_1_set_component" ],
     [ "Component", "classonion_1_1_component.html", "classonion_1_1_component" ],
+    [ "ComponentData", "classonion_1_1_component_data.html", "classonion_1_1_component_data" ],
     [ "CreateOperator", "classonion_1_1_create_operator.html", "classonion_1_1_create_operator" ],
     [ "NonCopyable", "classonion_1_1_non_copyable.html", "classonion_1_1_non_copyable" ],
-    [ "RandomEngineProxy", "classonion_1_1_random_engine_proxy.html", "classonion_1_1_random_engine_proxy" ],
     [ "RandomEngine", "classonion_1_1_random_engine.html", "classonion_1_1_random_engine" ],
+    [ "RandomEngineProxy", "classonion_1_1_random_engine_proxy.html", "classonion_1_1_random_engine_proxy" ],
     [ "RandomLegacyC", "classonion_1_1_random_legacy_c.html", "classonion_1_1_random_legacy_c" ],
     [ "RandomSTL", "classonion_1_1_random_s_t_l.html", "classonion_1_1_random_s_t_l" ],
-    [ "operator<<", "namespaceonion.html#af909f3a6d68f3af85279d11b10122954", null ],
-    [ "operator<<", "namespaceonion.html#a35c1d2c9776d4df38a81ee2bca6cc105", null ],
-    [ "Random", "namespaceonion.html#a110142f2ca38670c3cf0304e557fedfd", null ],
-    [ "SetRandomEngine", "namespaceonion.html#ae23407f6fc77f1bb2a9b6fc3c2c42dfc", null ]
+    [ "SetComponent", "classonion_1_1_set_component.html", "classonion_1_1_set_component" ]
 ];
